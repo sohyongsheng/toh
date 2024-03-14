ea1762047c68f512c61681e9fee743c9e62820c3
Soh Yong Sheng <sohyongsheng@gmail.com>
2024-03-14 16:08:52 +0800

    Finish implementation

---
+++ readme.md
@@ -0,0 +1,19 @@
+[Tower of Hanoi][toh-wiki]
+==========================
+
+  [toh-wiki]: https://en.wikipedia.org/wiki/Tower_of_Hanoi
+
+Run `python run.py`. We'll see something like this:
+
+    Step: 0
+        A: 3 2 1
+        B:
+        C:
+    ...
+
+- A, B and C are the three pegs.
+- The numbers represent the size of the disks.
+- Our goal is to move all disks from peg A to peg C.
+
+Modify the code accordingly to change the initial number of disks.
+
--- run.py
+++ run.py
@@ -1,24 +1,4 @@
-class Peg:
-    def __init__(self, name):
-        self.name = name
-        self.disks = []
-
-    def __str__(self):
-        disks = ' '.join(str(disk) for disk in self.disks)
-        return f'{self.name}: {disks}'
-
-    def add(self, disk):
-        if self.disks:
-            top_disk = self.disks[-1]
-            assert disk.size < top_disk.size
-        self.disks.append(disk)
-
-    def remove(self):
-        return self.disks.pop()
-
-    def move(self, peg):
-        disk = self.remove()
-        peg.add(disk)
+import textwrap
 
 class Disk:
     def __init__(self, size):
@@ -27,19 +7,82 @@ class Disk:
     def __str__(self):
         return str(self.size)
 
+class Peg:
+    def __init__(self, game = None):
+        self.game = game
+        self.disks = []
+
+    def __str__(self):
+        return ' '.join(str(disk) for disk in self.disks)
+
+    def add(self, disk):
+        if self.disks:
+            top_disk = self.disks[-1]
+            assert disk.size < top_disk.size
+        self.disks.append(disk)
+
+    def remove(self):
+        assert self.disks
+        return self.disks.pop()
+
+    def move(self, target):
+        disk = self.remove()
+        target.add(disk)
+        if self.game is not None:
+            self.game.step += 1
+            print(self.game)
+
+class TowerOfHanoi:
+    def __init__(self, num_disks):
+        self.pegs = {
+            'A': Peg(self),
+            'B': Peg(self),
+            'C': Peg(self),
+        }
+        source = self.pegs['A']
+        for i in range(num_disks, 0, -1):
+            disk = Disk(i)
+            source.add(disk)
+        self.step = 0
+
+    def __str__(self):
+        prefix = f"Step: {self.step}"
+        s = '\n'.join(
+            f"{k}: {v}"
+            for (k, v) in self.pegs.items()
+        )
+        s = textwrap.indent(s, 4 * ' ')
+        return f"{prefix}\n{s}"
+
+    def move(self, num_disks, source, target, spare):
+        if num_disks == 1:
+            source.move(target)
+        else:
+            self.move(
+                num_disks - 1,
+                source,
+                target = spare, 
+                spare = target,
+            )
+            source.move(target)
+            self.move(
+                num_disks - 1,
+                source = spare,
+                target = target, 
+                spare = source,
+            )
+
+    def solve(self):
+        print(self)
+        source = self.pegs['A']
+        self.move(
+            len(source.disks), 
+            source, 
+            target = self.pegs['C'], 
+            spare = self.pegs['B'],
+        )
+
 if __name__ == '__main__':
-    a = Peg('A')
-    num_disks = 3
-    for i in range(num_disks, 0, -1):
-        disk = Disk(i)
-        a.add(disk)
-    b = Peg('B')
-    print(a)
-    print(b)
-    a.move(b)
-    print(a)
-    print(b)
-
-
-
+    toh = TowerOfHanoi(num_disks = 3)
+    toh.solve()
 
